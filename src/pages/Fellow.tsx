946e775c8810f12c79865225f187c233a52267fd
ayubaezekiel <muntech@protonmail.com>
2024-02-05 13:55:51 +0100

    vercel commit

--- src/pages/Fellow.tsx
+++ src/pages/Fellow.tsx
@@ -1,187 +1,196 @@
+import { PageBreadcrumbs } from "../components/Breadcrumbs";
+
 export default function S() {
   return (
-    <section className="inner-page">
-      <div className="container">
-        <h1>
-          ENROLMENT INTO POST FELLOWSHIP PROGRAM IN URETHRAL AND GENITAL
-          RECONSTRUCTION
-        </h1>
-        <h2>Training Program, Goals And Objectives</h2>
-        <p>
-          The goal of our Post fellowship programme is to provide subspecialty
-          training in Urethral and Genital Reconstruction. The Post-Fellowship
-          Program has been designed to help Fellows refine their surgical skills
-          in Urethral and Genital Reconstruction to function as a valued member
-          of Genitourinary Reconstructive Societies, globally. Fellows will
-          acquire more specialized expertise that complement their residency
-          training.
-        </p>
-        <p>The objectives are:</p>
-        <ul>
-          <li>
-            To assist fellows develop competence in surgical techniques of
-            anterior and posterior urethroplasty;
-          </li>
-          <li>
-            To train fellows in the evaluation, decision making and surgical
-            management of adults and children with uretheral anomalies and
-            trauma
-          </li>
-          <li>
-            To acquire surgical skills necessary in the reconstruction of a
-            variety of intersex conditions.{" "}
-          </li>
-        </ul>
-        <p>
-          They will also have opportunity to participate in research on
-          individuals with disorders of sex development, for which the Institute
-          has a grant
-        </p>
-        <h2>MODE OF DELIVERY</h2>
-        <ul>
-          <li>
-            Full and active participate in evaluation of patients and decision
-            making{" "}
-          </li>
-          <li>
-            Clinical meetings and discussions, conferences, lectures and
-            seminars on relevant anatomy of the genitalia and urethral
-            substitutes, tissue engineering and future of urethral
-            reconstruction
-          </li>
-          <li>
-            Fellows will actively participate and develop competence in surgical
-            procedures including, but not limited to, Urethrotomy and other
-            Minimally Invasive Interventions for Urethral Stricture, Current
-            Anterior urethroplasty, Posterior Urethroplasty, Feminizing
-            genitoplasty, metoidioplasty, Colovaginoplasty , Gonadectomy&
-            Hysterocolpectomy, Hypospadias repair, and Vesico-Vaginal Fistula
-            repair
-          </li>
-          <li>
-            Trainees surgery recording will be encouraged for video reviews with
-            the trainers
-          </li>
-          <li>
-            Fellows are required to participate in at least 1 clinic day and 1
-            operation session per week, academic meetings and must present 2
-            seminars on contemporary issues relevant to the training before
-            completion
-          </li>
-          <li>
-            There will be opportunity of DSD and tissue engineering research and
-            publication
-          </li>
-        </ul>
+    <div>
+      <PageBreadcrumbs
+        currentUrl={[
+          { href: "/", title: "Home" },
+          { href: "/contact/", title: "Urethral And Genetal Reconstruction" },
+        ]}
+        title="Post Fellowship Program"
+      />
+      <section className="inner-page">
+        <div className="container">
+          <h1 className="mt-4">
+            ENROLMENT INTO POST FELLOWSHIP PROGRAM IN URETHRAL AND GENITAL
+            RECONSTRUCTION
+          </h1>
+          <h2>Training Program, Goals And Objectives</h2>
+          <p>
+            The goal of our Post fellowship programme is to provide subspecialty
+            training in Urethral and Genital Reconstruction. The Post-Fellowship
+            Program has been designed to help Fellows refine their surgical
+            skills in Urethral and Genital Reconstruction to function as a
+            valued member of Genitourinary Reconstructive Societies, globally.
+            Fellows will acquire more specialized expertise that complement
+            their residency training.
+          </p>
+          <p>The objectives are:</p>
+          <ul>
+            <li>
+              To assist fellows develop competence in surgical techniques of
+              anterior and posterior urethroplasty;
+            </li>
+            <li>
+              To train fellows in the evaluation, decision making and surgical
+              management of adults and children with uretheral anomalies and
+              trauma
+            </li>
+            <li>
+              To acquire surgical skills necessary in the reconstruction of a
+              variety of intersex conditions.{" "}
+            </li>
+          </ul>
+          <p>
+            They will also have opportunity to participate in research on
+            individuals with disorders of sex development, for which the
+            Institute has a grant
+          </p>
+          <h2>MODE OF DELIVERY</h2>
+          <ul>
+            <li>
+              Full and active participate in evaluation of patients and decision
+              making{" "}
+            </li>
+            <li>
+              Clinical meetings and discussions, conferences, lectures and
+              seminars on relevant anatomy of the genitalia and urethral
+              substitutes, tissue engineering and future of urethral
+              reconstruction
+            </li>
+            <li>
+              Fellows will actively participate and develop competence in
+              surgical procedures including, but not limited to, Urethrotomy and
+              other Minimally Invasive Interventions for Urethral Stricture,
+              Current Anterior urethroplasty, Posterior Urethroplasty,
+              Feminizing genitoplasty, metoidioplasty, Colovaginoplasty ,
+              Gonadectomy& Hysterocolpectomy, Hypospadias repair, and
+              Vesico-Vaginal Fistula repair
+            </li>
+            <li>
+              Trainees surgery recording will be encouraged for video reviews
+              with the trainers
+            </li>
+            <li>
+              Fellows are required to participate in at least 1 clinic day and 1
+              operation session per week, academic meetings and must present 2
+              seminars on contemporary issues relevant to the training before
+              completion
+            </li>
+            <li>
+              There will be opportunity of DSD and tissue engineering research
+              and publication
+            </li>
+          </ul>
 
-        <h2>POST-FELLOWSHIP FORMAT</h2>
-        <ul>
-          <li>Has a duration of 12 consecutive months – starting in July</li>
-          <li>Training is full time </li>
-          <li>
-            Fellows will register with the Institute and the Teaching Hospital
-            and will be required to sign an undertaking to abide by regulation
-            of the training and the Hospital
-          </li>
-        </ul>
+          <h2>POST-FELLOWSHIP FORMAT</h2>
+          <ul>
+            <li>Has a duration of 12 consecutive months – starting in July</li>
+            <li>Training is full time </li>
+            <li>
+              Fellows will register with the Institute and the Teaching Hospital
+              and will be required to sign an undertaking to abide by regulation
+              of the training and the Hospital
+            </li>
+          </ul>
 
-        <h2>EVALUATION</h2>
-        <p>
-          Successful fellows must be fully documented at the Institute and The
-          Usmanu Danfodiyo University Teaching Hospital. A log book will be made
-          available to fellows. Evaluation throughout the Fellowship period will
-          include Formal oral evaluations every 3 months throughout the course
-          of the training year. These evaluations will include an active
-          discussion of the evaluation itself, and the Fellow’s areas of
-          strength and weakness. Any serious concerns discussed with the Fellows
-          will be addressed. Fellows are expected to keep a logbook of
-          procedures assisted and performed. The trainee may be asked to
-          disengage if he/she does not meet the requirement satisfactorily as
-          judged by a panel of assessors and this will be dully communicated to
-          the primary employer.
-        </p>
+          <h2>EVALUATION</h2>
+          <p>
+            Successful fellows must be fully documented at the Institute and The
+            Usmanu Danfodiyo University Teaching Hospital. A log book will be
+            made available to fellows. Evaluation throughout the Fellowship
+            period will include Formal oral evaluations every 3 months
+            throughout the course of the training year. These evaluations will
+            include an active discussion of the evaluation itself, and the
+            Fellow’s areas of strength and weakness. Any serious concerns
+            discussed with the Fellows will be addressed. Fellows are expected
+            to keep a logbook of procedures assisted and performed. The trainee
+            may be asked to disengage if he/she does not meet the requirement
+            satisfactorily as judged by a panel of assessors and this will be
+            dully communicated to the primary employer.
+          </p>
 
-        <h2>FELLOWSHIP BENEFITS</h2>
-        <ul>
-          <li>free accommodation throughout the training</li>
-          <li>Free lunch in Operation Rooms</li>
-          <li>Donation of a Urethroplasty Instrument Set</li>
-          <li>
-            A certificate of training issued by the Institute and a
-            recommendation for membership of Society for Genitourinary
-            Reconstructive Surgeons(GURS)
-          </li>
-        </ul>
-        <h2>APPLICATION PROCESS</h2>
-        <p>
-          Selection is competitive and open to Urologists who have met the
-          following requirements
-        </p>
-        <ul>
-          <li>
-            Applicants should not be more then 50 years by 1st July of the
-            enrollment year
-          </li>
-          <li>
-            Must possess Fellowship of the National Postgraduate Medical
-            College, or the West African College of Surgeons or an equivalent
-            qualification
-          </li>
-          <li>
-            Registration with Medical and Dental Council of Nigeria and an
-            active practicing license
-          </li>
-          <li>
-            Letter from applicant outlining professional goals and how this
-            Fellowship meets those goals
-          </li>
-          <li>Curriculum Vitae </li>
-          <li>
-            Two Letters of Recommendation, one of which must be from a Fellow in
-            in Urology{" "}
-          </li>
-          <li>
-            Letter of sponsorship for supernumerary post fellowship training
-            from primary employer. This is a supernumerary training and
-            prospective fellows most show evidence of employment{" "}
-          </li>
-        </ul>
+          <h2>FELLOWSHIP BENEFITS</h2>
+          <ul>
+            <li>free accommodation throughout the training</li>
+            <li>Free lunch in Operation Rooms</li>
+            <li>Donation of a Urethroplasty Instrument Set</li>
+            <li>
+              A certificate of training issued by the Institute and a
+              recommendation for membership of Society for Genitourinary
+              Reconstructive Surgeons(GURS)
+            </li>
+          </ul>
+          <h2>APPLICATION PROCESS</h2>
+          <p>
+            Selection is competitive and open to Urologists who have met the
+            following requirements
+          </p>
+          <ul>
+            <li>
+              Applicants should not be more then 50 years by 1st July of the
+              enrollment year
+            </li>
+            <li>
+              Must possess Fellowship of the National Postgraduate Medical
+              College, or the West African College of Surgeons or an equivalent
+              qualification
+            </li>
+            <li>
+              Registration with Medical and Dental Council of Nigeria and an
+              active practicing license
+            </li>
+            <li>
+              Letter from applicant outlining professional goals and how this
+              Fellowship meets those goals
+            </li>
+            <li>Curriculum Vitae </li>
+            <li>
+              Two Letters of Recommendation, one of which must be from a Fellow
+              in in Urology{" "}
+            </li>
+            <li>
+              Letter of sponsorship for supernumerary post fellowship training
+              from primary employer. This is a supernumerary training and
+              prospective fellows most show evidence of employment{" "}
+            </li>
+          </ul>
 
-        <p>
-          {" "}
-          <strong> Note: Only one slot available per year </strong>{" "}
-        </p>
-        <h2>This information must be forwarded via E-Mail to:</h2>
-        <p>
-          Prof Ismaila A Mungadi <br />
-          FWACS, FMAS, Cert Endourol, Member GURS
-        </p>
-
-        <p>
-          {" "}
-          Director, TETFund Centre of Excellence in Urology & Nephrology
-          Institute of Urology and Nephrology.Usmanu Danfodiyo University ,
-          Sokoto
-        </p>
-        <a href="imungadi@yahoo.com"> imungadi@yahoo.com</a>
-        <p>
-          <a href="arzika.ismaila@udusok.ed.ng ">
+          <p>
             {" "}
-            arzika.ismaila@udusok.ed.ng{" "}
-          </a>
-        </p>
-        <p>copy: tce-un@udusok.edu.ng </p>
+            <strong> Note: Only one slot available per year </strong>{" "}
+          </p>
+          <h2>This information must be forwarded via E-Mail to:</h2>
+          <p>
+            Prof Ismaila A Mungadi <br />
+            FWACS, FMAS, Cert Endourol, Member GURS
+          </p>
 
-        <p>
-          {" "}
-          <strong>
+          <p>
             {" "}
-            Closing Date: 30th March,2023
-            <br />
-            Training commencement date 1st July, 2023
-          </strong>
-        </p>
-      </div>
-    </section>
+            Director, TETFund Centre of Excellence in Urology & Nephrology
+            Institute of Urology and Nephrology.Usmanu Danfodiyo University ,
+            Sokoto
+          </p>
+          <a href="imungadi@yahoo.com"> imungadi@yahoo.com</a>
+          <p>
+            <a href="arzika.ismaila@udusok.ed.ng ">
+              {" "}
+              arzika.ismaila@udusok.ed.ng{" "}
+            </a>
+          </p>
+          <p>copy: tce-un@udusok.edu.ng </p>
+
+          <p>
+            <strong>
+              Closing Date: 30th March,2023
+              <br />
+              Training commencement date 1st July, 2023
+            </strong>
+          </p>
+        </div>
+      </section>
+    </div>
   );
 }
